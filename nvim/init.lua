2ee4e54677f8150c52631cb6c9279584c8979d46
melMass <mel@melmassadian.com>
2024-02-28 22:43:11 +0100

    ⚡ feat: add base config
    wip

---
+++ nvim/init.lua
@@ -0,0 +1,76 @@
+--NOTE: this is an e2e demo configuration to add support for nushell inside neovim.
+-- inspired by kickstart.nvim the idea is to show an example you can adapt to your own config.
+
+--ISSUE:: todo...
+
+--INFO: Bootstrapping lazy (the package manager)
+-- See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+-- INFO: settings to set nushell as the shell
+vim.opt.shellredir = "--stdin < %s"
+vim.opt.shellcmdflag = "-c"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = " "
+vim.opt.shellxescape = ""
+vim.opt.sh = "nu"
+
+-- NOTE: you can instead uncomment the following to for instance provide custom config paths
+-- depending on the OS
+
+-- utility method to detect the OS, if you use a custom config the following can be handy
+-- local function getOS()
+--   if jit then
+--     return jit.os
+--   end
+--   local fh, err = assert(io.popen('uname -o 2>/dev/null', 'r'))
+--   if fh then
+--     Osname = fh:read()
+--   end
+--
+--   return Osname or 'Windows'
+-- end
+
+-- if getOS() == 'Windows' then
+--   vim.opt.sh = 'nu --env-config C:/Users/User/.dot/env/env.nu --config C:/Users/User/.dot/env/config.nu'
+-- else
+--   vim.opt.sh = 'nu --env-config /Users/mel/.dot/env/env.nu --config /Users/mel/.dot/env/config.nu'
+-- end
+
+require("lazy").setup({
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("tokyonight-night")
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+
+		dependencies = "nushell/tree-sitter-nu",
+		config = function()
+			-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+			---@diagnostic disable-next-line: missing-fields
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"nu",
+					"vim",
+					"vimdoc",
+				},
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+}, {})
