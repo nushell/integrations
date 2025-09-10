7d7e1afd44edcf1908df080c17a382b87bbc41d2
Taylor C. Richberger <taylor@axfive.net>
2025-09-09 22:33:10 -0600

    fix: Ensure config directory exists in post-install and pre-remove (#70)
    This was causing install to fail for rpm-ostree install.

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -2,4 +2,17 @@
 # Author: hustcer
 # Created: 2025/02/25 18:55:20
 
+set -e
+
+# Running nu in the rpm-ostree script environment fails if the config directory
+# doesn't exist, preventing the post-install.nu script from being run.
+if [ -n "${XDG_CONFIG_HOME+x}" ]; then
+	mkdir -p "$XDG_CONFIG_HOME"
+elif [ -n "${HOME+x}" ]; then
+	mkdir -p "$HOME/.config"
+else
+	export XDG_CONFIG_HOME="$PWD/.config"
+	mkdir -p "$XDG_CONFIG_HOME"
+fi
+
 nu /usr/libexec/nushell/post-install.nu
--- scripts/pre-remove.sh
+++ scripts/pre-remove.sh
@@ -2,4 +2,17 @@
 # Author: hustcer
 # Created: 2025/03/10 08:35:20
 
+set -e
+
+# Running nu in the rpm-ostree script environment fails if the config directory
+# doesn't exist, preventing the pre-remove.nu script from being run.
+if [ -n "${XDG_CONFIG_HOME+x}" ]; then
+	mkdir -p "$XDG_CONFIG_HOME"
+elif [ -n "${HOME+x}" ]; then
+	mkdir -p "$HOME/.config"
+else
+	export XDG_CONFIG_HOME="$PWD/.config"
+	mkdir -p "$XDG_CONFIG_HOME"
+fi
+
 nu /usr/libexec/nushell/pre-remove.nu
