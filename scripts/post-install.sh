56b17540d54a48aa014657af72315759278cf4e3
Taylor C. Richberger <taylor@axfive.net>
2025-09-09 19:31:33 -0600

    fix: Ensure config directory exists in post-install and pre-remove
    This was causing install to fail for rpm-ostree install.

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -2,4 +2,13 @@
 # Author: hustcer
 # Created: 2025/02/25 18:55:20
 
+if [ -n "${XDG_CONFIG_HOME+x}" ]; then
+	mkdir -p "$XDG_CONFIG_HOME"
+elif [ -n "${HOME+x}" ]; then
+	mkdir -p "$HOME/.config"
+else
+	export XDG_CONFIG_HOME="$PWD/.config"
+	mkdir -p "$XDG_CONFIG_HOME"
+fi
+
 nu /usr/libexec/nushell/post-install.nu
--- scripts/pre-remove.sh
+++ scripts/pre-remove.sh
@@ -2,4 +2,13 @@
 # Author: hustcer
 # Created: 2025/03/10 08:35:20
 
+if [ -n "${XDG_CONFIG_HOME+x}" ]; then
+	mkdir -p "$XDG_CONFIG_HOME"
+elif [ -n "${HOME+x}" ]; then
+	mkdir -p "$HOME/.config"
+else
+	export XDG_CONFIG_HOME="$PWD/.config"
+	mkdir -p "$XDG_CONFIG_HOME"
+fi
+
 nu /usr/libexec/nushell/pre-remove.nu
