95545c9c903d8361c9059d760eb8fe5a4be17299
Taylor C. Richberger <taylor@axfive.net>
2025-09-09 19:37:04 -0600

    fix: add set -e and explanatory comment to install scripts

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -2,6 +2,10 @@
 # Author: hustcer
 # Created: 2025/02/25 18:55:20
 
+set -e
+
+# Running nu in the rpm-ostree script environment fails if the config directory
+# doesn't exist, preventing the post-install.nu script from being run.
 if [ -n "${XDG_CONFIG_HOME+x}" ]; then
 	mkdir -p "$XDG_CONFIG_HOME"
 elif [ -n "${HOME+x}" ]; then
